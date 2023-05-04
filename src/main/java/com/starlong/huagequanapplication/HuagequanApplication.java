9b1afb10ff44e0968a31dfbbf2c9b1fffb40fccc
Ming momcoco <2554868271@qq.com>
2023-05-04 15:04:53 +0800

    初始化

---
+++ src/main/java/com/starlong/huagequanapplication/HuagequanApplication.java
@@ -0,0 +1,13 @@
+package com.starlong.huagequanapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuagequanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuagequanApplication.class, args);
+    }
+
+}
